616e2d7a086904f901cd057d60facb56826919d2
Wiluś Osada <235940@edu.p.lodz.pl>
2022-05-27 12:34:53 +0200

    ogolne przerobienie klasy stosu i klasy karty

--- src/com/company/Card.java
+++ src/com/company/Card.java
@@ -10,27 +10,58 @@ import java.util.Map;
 
 public class Card {
 
-    private String name;
+    private static int OVERHEALTH = 5000;
+    private final String name;
     private int attack;
     private int hp;
     private int mana_cost;
 
-    public Card(int record){
-        try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bakugan","root","");        //connecting with local database
-            Statement statement = connection.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM bacard WHERE bacard.bakuID = "+ record);     //passing our query through the statement
-            while (rs.next()){
-                name = rs.getString("name");
-                attack = rs.getInt("attack");
-                hp = rs.getInt("hp");
-                mana_cost = rs.getInt("mana");
-            }
-            statement.close();          //closing the conversation with database.
-            connection.close();             //optional
-        }catch (Exception e){
-            System.out.println("Error: loading from database\n");
-            e.printStackTrace();
+    public String getName() {
+        return name;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getMana_cost() {
+        return mana_cost;
+    }
+
+    //returns true if bakugan died as a result else returns false
+    public boolean damage(int ammount)
+    {
+        if(hp - ammount >= OVERHEALTH)  return true;
+        if(hp - ammount <= 0)
+        {
+            hp = 0;
+            return true;
         }
+        return false;
+    }
+
+    //returns true if bakugan died as a result else returns false
+    public boolean heal(int ammount)
+    {
+        if(hp + ammount >= OVERHEALTH)  return true;
+        if(hp + ammount <= 0)
+        {
+            hp = 0;
+            return true;
+        }
+        return false;
+    }
+
+    //constructor
+    public Card(String n, int a, int h, int m)
+    {
+        name =n;
+        attack = a;
+        hp = h;
+        mana_cost = m;
     }
 }
