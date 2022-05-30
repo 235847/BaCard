5ace8a3bd5458a56c79113d4b250e0daee947e79
235847 <235847@edu.p.lodz.pl>
2022-05-30 11:49:23 +0200

    Database correction + description

--- src/main/java/com/example/bacard/InsertsIntoDatabase.java
+++ src/main/java/com/example/bacard/InsertsIntoDatabase.java
@@ -5,16 +5,53 @@ import java.sql.DriverManager;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.Scanner;
 
 public class InsertsIntoDatabase {
 
+    private final static String QUERY_CREATE_TABLE = "CREATE TABLE bacard ("+
+            "bakuID int NOT NULL," +
+            "name varchar(100) NOT NULL," +
+            "attack int NOT NULL," +
+            "hp int NOT NULL," +
+            "mana int NOT NULL," +
+            "PRIMARY KEY (bakuID))";
+
+    private final static String QUERY_INSERT = " insert into bacard (bakuID, name, attack, hp, mana)"
+            + " values (?, ?, ?, ?, ?)";
+
+
+    /**
+     * @description
+     * {
+     *     To load this database we need to download the XAMPP package and set default settings during an installation process.
+     *     Then we are required to start the application and click "START" for APACHE and MYSQL.
+     *     Last but not least, we must enter the phpmyadmin by writing in our browser: localhost/phpmyadmin/ (enter).
+     * }
+     * @section CREATE CONNECTION - explanation:
+     * {
+     *     jdbc:mysql:// - means we want to activate jdbc with mysql.
+     *     localhost/ - our database is set on our computer.
+     *     bakugan - name of database.
+     *     root - by default the user is named as root.
+     *     "" - no password protection.
+     * }
+     * @section CREATE TABLE - creating table bacard.
+     * @section INSERTING - we want to insert data from the file into bacard table.
+     */
     public static void main(String[] args) {
         try{
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/bakugan","root","");
-            String query = " insert into bacard (bakuID, name, attack, hp, mana)"
-                    + " values (?, ?, ?, ?, ?)";
-            PreparedStatement preparedStmt = connection.prepareStatement(query);
+            /*CREATE CONNECTION*/
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/bakugan","root","");     //establishing connection with database
+
+            /*CREATE TABLE*/
+            PreparedStatement prepareStatement_for_creating = connection.prepareStatement(QUERY_CREATE_TABLE);
+            prepareStatement_for_creating.executeUpdate();
+            prepareStatement_for_creating.close();      //closing the statement.
+
+            /*INSERTING*/
+            PreparedStatement preparedStmt = connection.prepareStatement(QUERY_INSERT);
             File fp = new File("inserts");
             Scanner scanner = new Scanner(fp);
             int i = 1;
@@ -47,6 +84,7 @@ public class InsertsIntoDatabase {
         }catch (FileNotFoundException e){
             System.out.println("Error: couldn't open the file");
         }catch (Exception e){
+            e.printStackTrace();
             System.out.println("Error: writing into database");
         }
     }
