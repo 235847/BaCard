5e35a93b0d904890e1ee2d06952c4e98e802fa7b
Wiluś Osada <235940@edu.p.lodz.pl>
2022-06-07 19:09:27 +0200

    fully functional gameplay

--- src/main/java/com/example/bacard/ArenaController.java
+++ src/main/java/com/example/bacard/ArenaController.java
@@ -82,36 +82,119 @@ public class ArenaController implements Initializable {
         Stage window = (Stage) ((Node)event.getSource()).getScene().getWindow();
 
         if(Actround%2==0){
-            if(player1.getPlayer_deck().size()<7){
-                player1.addCard(deck.drawCard());
-                player1.setCardView();
-                player1.setCurrent_mana(maxRoundMana);
-            }
+            player1.can_move = false;
+            player2.can_move = true;
+            Actround++;
         }
-        else{
-            if(player2.getPlayer_deck().size()<7){
-                player2.addCard(deck.drawCard());
-                player2.setCardView();
-                player2.setCurrent_mana(maxRoundMana);
+        else {
+            int ammount1 = get_ready_num(player1);
+            int ammount2 = get_ready_num(player2);
+            if(ammount1 >= ammount2)
+            {
+                fight(player1,player2,ammount1,ammount2);
             }
+            else
+            {
+                fight(player2,player1,ammount2,ammount1);
+            }
+            player1.can_move = true;
+            player2.can_move = false;
+            maxRoundMana++;
+            Actround++;
+            round_end_addcards();
+            valueRound.setText(Integer.toString(Actround/2));
+            this.manaPlayer1.setText(Integer.toString(this.player1.getCurrent_mana()));
+            this.hpPlayer1.setText(Integer.toString(this.player1.getHp()));
+            this.manaPlayer2.setText(Integer.toString(this.player2.getCurrent_mana()));
+            this.hpPlayer2.setText(Integer.toString(this.player2.getHp()));
         }
-        maxRoundMana++;
-        Actround++;
-        valueRound.setText(Integer.toString(Actround));
-        this.manaPlayer1.setText(Integer.toString(this.player1.getCurrent_mana()));
-        this.hpPlayer1.setText(Integer.toString(this.player1.getHp()));
-        this.manaPlayer2.setText(Integer.toString(this.player2.getCurrent_mana()));
-        this.hpPlayer2.setText(Integer.toString(this.player2.getHp()));
 
-
-        if(Actround >=20){
+        if(Actround >= 40){
             display("Game Over","Player 1 HP :" + player1.getHp() + "\nPlayer 2 HP :" + player2.getHp());
             window.close();
         }
 
 
     }
+    public boolean remove_if_dead(Card card)
+    {
+        if(card.is_dead)
+        {
+            MainStage.getChildren().remove(card.pane);
+            if(card.getPlayerId() == 1)
+            {
+                player1.getPlayer_deck().remove(card);
+            }
+            else
+            {
+                player2.getPlayer_deck().remove(card);
+            }
+            return true;
+        }
+        return false;
+    }
 
+    public void fight(Player more, Player less, int more_num, int less_num)
+    {
+        Card card1,card2;
+        int i;
+        for(i = 0 ; i < less_num ; i++)
+        {
+            card1 = more.get_next_ready(i);
+            card2 = less.get_next_ready(i);
+            card1.attack(card2);
+        }
+        for( ; i < more_num ; i++)
+        {
+            less.setHp(less.getHp() - more.get_next_ready(i).getAttack());
+        }
+        for(int j = 0 ; j < more.getPlayer_deck().size() ; j++)
+        {
+            if(remove_if_dead(more.getPlayer_deck().get(j)))
+            {
+                j = 0;
+            }
+        }
+        for(int j = 0 ; j < less.getPlayer_deck().size() ; j++)
+        {
+            if(remove_if_dead(less.getPlayer_deck().get(j)))
+            {
+                j = 0;
+            }
+        }
+        more.reset_ready_states();
+        less.reset_ready_states();
+        if(less.getHp() <= 0)
+        {
+            display("Game Over","Player 1 HP :" + player1.getHp() + "\nPlayer 2 HP :" + player2.getHp());
+        }
+    }
+    public int get_ready_num(Player player)
+    {
+        int res = 0;
+        for(int i = 0 ; i < player.getPlayer_deck().size() ; i++)
+        {
+            if(player.getPlayer_deck().get(i).ready == 1)
+            {
+                res++;
+            }
+        }
+        return res;
+    }
+
+    private void round_end_addcards()
+    {
+        if(player1.getPlayer_deck().size()<7){
+            player1.addCard(deck.drawCard());
+        }
+        if(player2.getPlayer_deck().size()<7) {
+            player2.addCard(deck.drawCard());
+        }
+            player1.setCardView();
+            player1.setCurrent_mana(maxRoundMana);
+            player2.setCardView();
+            player2.setCurrent_mana(maxRoundMana);
+    }
 
     public static void display(String title, String message) {
         Stage window = new Stage();
--- src/main/java/com/example/bacard/Card.java
+++ src/main/java/com/example/bacard/Card.java
@@ -5,6 +5,9 @@
 
 package com.example.bacard;
 
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+
 public class Card {
     private static final int OVERHEALTH = 5000;
     private final String name;
@@ -13,6 +16,9 @@ public class Card {
     private final int mana_cost;
     public int ready = 0;
     private int playerId;
+    public boolean is_dead = false;
+    public Label hp_label;
+    public AnchorPane pane;
 
     public void setPlayerId(int playerId) {
         this.playerId = playerId;
@@ -45,25 +51,21 @@ public class Card {
         return this.mana_cost;
     }
 
-    public boolean damage(int amount) {
-        if (this.hp - amount >= 5000) {
-            return true;
-        } else if (this.hp - amount <= 0) {
-            this.hp = 0;
-            return true;
-        } else {
-            return false;
-        }
+    public void attack(Card enemy)
+    {
+        System.out.println(this.getName() + "Attacked " + enemy.getName());
+        reduce_hp(enemy.getAttack());
+        enemy.reduce_hp(attack);
     }
 
-    public boolean heal(int amount) {
-        if (this.hp + amount >= 5000) {
-            return true;
-        } else if (this.hp + amount <= 0) {
-            this.hp = 0;
-            return true;
-        } else {
-            return false;
+    public void reduce_hp(int amount)
+    {
+        hp -= amount;
+        if(hp <= 0)
+        {
+            is_dead = true;
         }
+        hp_label.setText(Integer.toString(hp));
     }
+
 }
